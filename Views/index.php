c07814190147a64c9124f2903f9950dedd7f54c5
JoseDanielR15 <josedanielr163@gmail.com>
2026-02-01 01:24:00 -0600

    Primer commit
    Aqui se levanto el proyecto, se agrego el bootrap, tema de proyecto, css, js y demas archivos importantes
    
    Este es mas que todo haber puesto la base y editarla un poco para de ahi partir y editar las demas pantallas que faltan

---
+++ Views/index.php
@@ -0,0 +1,4 @@
+<?php
+    header("Location: Vinicio/inicio.php");
+    exit;
+?>
---
+++ Views/vInicio/Inicio.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>InfinityTech</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="../assets/images/icons/favicon.png"/>
+
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="../assets/vendor/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+</head>
+
+<body class="animsition">
+
+<!-- ================= HEADER ================= -->
+<header class="header-v2">
+	<div class="container-menu-desktop trans-03">
+		<div class="wrap-menu-desktop">
+			<nav class="limiter-menu-desktop p-l-45">
+
+				<!-- Logo -->
+				<a href="index.html" class="logo">
+					<img src="../assets/images/icons/logo-01.png" alt="InfinityTech">
+				</a>
+
+				<!-- Menu -->
+				<div class="menu-desktop">
+					<ul class="main-menu">
+						<li class="active-menu"><a href="index.html">Home</a></li>
+						<li><a href="product.html">Shop</a></li>
+						<li><a href="about.html">About Us</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+
+				<!-- Icons -->
+				<div class="wrap-icon-header flex-w flex-r-m h-full">
+					<div class="flex-c-m h-full p-lr-19">
+
+						<!-- Search -->
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
+							<i class="zmdi zmdi-search"></i>
+						</div>
+
+						<!-- Cart -->
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart"
+							data-notify="2">
+							<i class="zmdi zmdi-shopping-cart"></i>
+						</div>
+
+						<!-- Settings -->
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11">
+							<i class="zmdi zmdi-settings"></i>
+						</div>
+
+					</div>
+				</div>
+
+			</nav>
+		</div>
+	</div>
+</header>
+
+<!-- ================= CART ================= -->
+<div class="wrap-header-cart js-panel-cart">
+	<div class="s-full js-hide-cart"></div>
+
+	<div class="header-cart flex-col-l p-l-65 p-r-25">
+		<div class="header-cart-title flex-w flex-sb-m p-b-8">
+			<span class="mtext-103 cl2">Your Cart</span>
+			<div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart">
+				<i class="zmdi zmdi-close"></i>
+			</div>
+		</div>
+
+		<div class="header-cart-content">
+			<p class="stext-102">Your cart is currently empty.</p>
+		</div>
+	</div>
+</div>
+
+<!-- ================= SEARCH MODAL ================= -->
+<div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
+	<div class="container-search-header">
+		<button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+			<i class="zmdi zmdi-close"></i>
+		</button>
+
+		<form class="wrap-search-header flex-w">
+			<button class="flex-c-m trans-04">
+				<i class="zmdi zmdi-search"></i>
+			</button>
+			<input class="plh3" type="text" placeholder="Search products...">
+		</form>
+	</div>
+</div>
+
+<!-- ================= SLIDER ================= -->
+<section class="section-slide">
+	<div class="wrap-slick1">
+		<div class="slick1">
+			<div class="item-slick1" style="background-image: url('../assets/images/slide-01.jpg');">
+				<div class="container h-full">
+					<div class="flex-col-l-m h-full p-t-100 p-b-30">
+						<span class="ltext-202 cl2">Latest Technology</span>
+						<h2 class="ltext-104 cl2 p-t-19 p-b-43">New Devices</h2>
+						<a href="product.html"
+							class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15">
+							Shop Now
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- ================= FOOTER ================= -->
+<footer class="bg3 p-t-75 p-b-32">
+	<div class="container">
+		<p class="stext-107 cl6 txt-center">
+			© 2026 InfinityTech. All rights reserved.
+		</p>
+	</div>
+</footer>
+
+<!-- ================= JS ================= -->
+<script src="../assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="../assets/vendor/animsition/js/animsition.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/popper.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/vendor/slick/slick.min.js"></script>
+<script src="../assets/js/main.js"></script>
+
+</body>
+</html>
+
