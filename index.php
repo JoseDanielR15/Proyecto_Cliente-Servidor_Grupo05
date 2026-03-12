5172b50d8c187d1c305ccea87bddcbb962d86156
Joel Lopez <davi-d57@hotmail.com>
2026-03-11 21:09:34 -0600

    Cambios en estructura

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    header("Location: Views/Vinicio/inicio.php");
+    exit;
+?>
+<?php
+    header("Location: Views/vsesion/sesion.php");
+    exit;
+?>
+<?php
+    header("Location: Views/vregistro/registro.php");
+    exit;
+?>
