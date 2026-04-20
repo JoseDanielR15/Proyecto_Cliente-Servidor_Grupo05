d4b2dfd262cdf207c86e2afb6f5e908667132187
JoseDanielR15 <josedanielr163@gmail.com>
2026-04-20 17:10:50 -0600

    Avance Cuadra
    De momento solamente arregle unos hypervinculos pero voy a hacer commit por que estuve probando lo de seguridad y me dio unos issues, entoces es para salvar eso

--- Views/vInicio/Inicio.php
+++ Views/vInicio/Inicio.php
@@ -200,7 +200,7 @@ $productos = ConsultarProductosController();
                 <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
                     <div class="block1 wrap-pic-w">
                         <img src='../assets/images/Monitores.jpg' alt="IMG-BANNER">
-                        <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                        <a href="/Proyecto_Cliente-Servidor_Grupo05/Views/vProductos/vProductosXCategoria.php?id_cat=4" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child1 flex-col-l">
                                 <span class="block1-name ltext-102 trans-04 p-b-8">Monitores</span>
                                 <span class="block1-info stext-102 trans-04"></span>
@@ -214,7 +214,7 @@ $productos = ConsultarProductosController();
                 <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
                     <div class="block1 wrap-pic-w" style='width: 88%;'>
                         <img src='../assets/images/Ofertas.jpg' alt="IMG-BANNER">
-                        <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                        <a href="/Proyecto_Cliente-Servidor_Grupo05/Views/vProductos/vProductosXCategoria.php?id_cat=1" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child1 flex-col-l">
                                 <span class="block1-name ltext-102 trans-04 p-b-8"></span>
                                 <span class="block1-info stext-102 trans-04"></span>
@@ -228,7 +228,7 @@ $productos = ConsultarProductosController();
                 <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
                     <div class="block1 wrap-pic-w">
                         <img src='../assets/images/Mouse.jpg' alt="IMG-BANNER">
-                        <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                        <a href="/Proyecto_Cliente-Servidor_Grupo05/Views/vProductos/vProductosXCategoria.php?id_cat=3" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child1 flex-col-l">
                                 <span class="block1-name ltext-102 trans-04 p-b-8">Perifericos</span>
                                 <span class="block1-info stext-102 trans-04"></span>
@@ -238,7 +238,7 @@ $productos = ConsultarProductosController();
                             </div>
                         </a>
                     </div>
-                </div>
+                </div>  
             </div>
         </div>
     </div>
